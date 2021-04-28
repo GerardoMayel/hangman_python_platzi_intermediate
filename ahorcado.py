9095e1709796d6dcda15434c5958850d36b8f1ca
Gerardo Mayel Fernández Alamilla <geramfernandez@gmail.com>
2021-04-28 01:36:52 -0400

    Including: Asking for name

--- ahorcado.py
+++ ahorcado.py
@@ -1,7 +1,12 @@
 
+# Import OS library to identify OS, for Lunix and Mac OS the name is 'posix'
+# For Windows os.name is 'nt'
+
 import os
 from os import system
 from os import name
+
+# Object Clean Shell Screen
 
 
 class clean_screen():
@@ -14,10 +19,28 @@ class clean_screen():
         else:
             _ = system('cls')
 
+# Object Capture player Nickname
+
+
+class player(clean_screen):
+    def __init__(self):
+        super().__init__()
+        self.name = name
+
+    def clean_screen(self):
+        super().clean()
+
+    def ask_name(self):
+        self.name = input('Enter your player nickname: ')
+        return self.name
+
 
 def run():
-    cleaner = clean_screen()
-    cleaner.clean()
+    #cleaner = clean_screen()
+    # cleaner.clean()
+    player_1 = player()
+    player_1.clean()
+    print(player_1.ask_name())
 
 
 if __name__ == '__main__':
